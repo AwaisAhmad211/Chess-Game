eb507fb6da509e3e0740a76a8389bb25b876d961
Awais Ahmad <itxawais211@gmail.com>
2024-07-03 19:38:52 -0700

    S

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const chess = new Chess();
 let players = {};
 let currentPlayer = "w";
 
-app.set("view engine", "ejs");
+app.set("view engine" , "ejs")
+app.set("views" , path.join(__dirname,"/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
